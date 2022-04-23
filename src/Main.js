f2a5de3691eca4d4f3b8fc6fe08564b3b1c90daf
Vasco21 <vascoeti123@gmail.com>
2022-04-24 00:55:38 +0200

    update

--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useIntl } from 'react-intl';
 import Switch from 'react-switch';
 import { FaHeart, FaBars } from 'react-icons/fa';
-import reactLogo from './assets/logo.svg';
+import reactLogo from './assets/logo.png';
 
 
 const Main = ({
@@ -22,28 +22,28 @@ const Main = ({
         </div>
         <header>
           <h1>
-            <img width={80} src={reactLogo} alt="react logo" /> {intl.formatMessage({ id: 'title' })}
+            <img width={80} src={reactLogo} alt="react logo" /> {intl.formatMessage({ id: 'Creative Power Developer' })}
           </h1>
-          <p>{intl.formatMessage({ id: 'description' })}</p>
+          <p>{intl.formatMessage({ id: 'With Creativity we can impliment new features and overcome our Dreams!!!' })}</p>
           <div className="social-bagdes">
             <a
-              href="https://github.com/azouaoui-med/react-pro-sidebar"
+              href="https://github.com/Vasco21/menu-app"
               target="_blank"
-              rel="noopener noreferrer"
+              rel="Vasco Eti"
             >
               <img
                 alt="GitHub stars"
-                src="https://img.shields.io/github/stars/azouaoui-med/react-pro-sidebar?style=social"
+                src="https://img.shields.io/github/stars/Vasco21/menu-app?style=social"
               />
             </a>
             <a
-              href="https://github.com/azouaoui-med/react-pro-sidebar"
+              href="https://github.com/Vasco21/menu-app"
               target="_blank"
-              rel="noopener noreferrer"
+              rel="Vasco Eti"
             >
               <img
                 alt="GitHub forks"
-                src="https://img.shields.io/github/forks/azouaoui-med/react-pro-sidebar?style=social"
+                src="https://img.shields.io/github/forks/Vasco21/menu-app?style=social"
               />
             </a>
           </div>
@@ -91,22 +91,22 @@ const Main = ({
         <footer>
           <small>
             © {new Date().getFullYear()} made with <FaHeart style={{ color: 'red' }} /> by -{' '}
-            <a target="_blank" rel="noopener noreferrer" href="https://azouaoui.netlify.com">
-              Mohamed Azouaoui
+            <a target="_blank" rel="noopener noreferrer" href="https://myportfolio-webapp.herokuapp.com">
+                Vasco Eti
             </a>
           </small>
           <br />
           <div className="social-bagdes">
-            <a href="https://github.com/azouaoui-med" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/Vasco21" target="_blank" rel="noopener noreferrer">
               <img
                 alt="GitHub followers"
-                src="https://img.shields.io/github/followers/azouaoui-med?label=github&style=social"
+                src="https://img.shields.io/github/followers/Vasco21?label=github&style=social"
               />
             </a>
-            <a href="https://twitter.com/azouaoui_med" target="_blank" rel="noopener noreferrer">
+            <a href="https://twitter.com/Qvary34" target="_blank" rel="noopener noreferrer">
               <img
                 alt="Twitter Follow"
-                src="https://img.shields.io/twitter/follow/azouaoui_med?label=twitter&style=social"
+                src="https://img.shields.io/twitter/follow/Vasco21?label=twitter&style=social"
               />
             </a>
           </div>
