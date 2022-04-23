7ce42500a5247139cc5a24b24d1503210a54b612
Vasco21 <vascoeti123@gmail.com>
2022-04-24 00:04:43 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { IntlProvider } from 'react-intl';
+import Layout from './Layout';
+
+import './styles/App.scss';
 
 function App() {
+  const [locale, setLocale] = useState('en');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IntlProvider locale={locale}>
+      <Layout setLocale={setLocale} />
+    </IntlProvider>
   );
 }
 
